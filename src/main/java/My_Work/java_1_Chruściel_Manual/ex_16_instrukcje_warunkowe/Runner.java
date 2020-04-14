c25425fec41d6792c95e113fbb177759ce54fb00
Tomasz Zakrzewski <tomasz.zakrzewski86@gmail.com>
2020-04-14 11:38:57 +0200

    Commit 11:40 14.04.2020

---
+++ src/main/java/My_Work/java_1_Chruściel_Manual/ex_16_instrukcje_warunkowe/Runner.java
@@ -0,0 +1,8 @@
+package My_Work.java_1_Chruściel_Manual.ex_16_instrukcje_warunkowe;
+
+public class Runner {
+    public static void main(String[] args) {
+        Condition condition = new Condition();
+        condition.getMonthsBy(2);
+    }
+}
--- src/test/java/Tests_ex_14_instrukcje_warunkowe/ConditionTest.java
+++ src/test/java/Tests_ex_14_instrukcje_warunkowe/ConditionTest.java
@@ -1,4 +1,49 @@
 package Tests_ex_14_instrukcje_warunkowe;
 
+import My_Work.java_1_Chruściel_Manual.ex_16_instrukcje_warunkowe.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ConditionTest {
+
+    private Condition condition;
+
+    @BeforeEach
+    void setUp() {
+        condition = new Condition();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenGivenNumberIsEven() {
+        // When
+         boolean result = condition.isEven(12);
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenGivenNumberIsOdd() {
+        // When
+        boolean result = condition.isOdd(11);
+        // Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void shouldReturnCorrectNumberWhenGivenNumberIsDivisebleBySeven() {
+        // When
+        int divisible = condition.divisible(49);
+        // Then
+        assertThat(divisible).isEqualTo(7);
+    }
+
+    @Test
+    public void shouldReturnCorrectMonthName() {
+        // When
+        String result = condition.getMonthNameBy(1);
+        // Then
+        assertThat(result).isEqualToIgnoringCase("January");
+    }
 }
